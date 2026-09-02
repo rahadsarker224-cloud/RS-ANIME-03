95669ac542e52d583a4b4fc2197533a6dbb521c0
rahadsarker224-cloud <rahadsarker224@gmail.com>
2026-09-02 22:41:46 +0600

    Upload: SplashActivity.kt

---
+++ app/src/main/kotlin/com/rsanime/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.rsanime
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
+        window.addFlags(FLAG_KEEP_SCREEN_ON)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000L)
+    }
+}
